f81f56b2dc5c7cd758567183ebcf4c6aba3b9ee8
Adam Govier <adam@govier-web.co.uk>
2021-06-15 18:46:14 +0100

    Further work on the meal planner / shortcuts

--- www/scripts/shortcuts.js
+++ www/scripts/shortcuts.js
@@ -1,9 +1,19 @@
 const exampleDataset = [
     {
         name: "Slice of pizza",
-        carbs: 35,
+        carbs: 28,
         img: "entry_pizza_140621.jpg"
-    }
+    },
+    {
+        name: "Macaroni Cheese",
+        carbs: 55,
+        img: "entry_MacaroniCheese_159621.jpg"
+    },
+    {
+        name: "Steak Pie",
+        carbs: 53,
+        img: "entry_SteakPie_150621.jpg"
+    },
 ]
 
 userData.save("shortcuts", JSON.stringify(exampleDataset));
@@ -14,11 +24,43 @@ loadLocalShortcuts();
 /**
  * @description Load shortcut data from local storage
  */
-function loadLocalShortcuts() {
-   JSON.parse(userData.get("shortcuts")).forEach(shortcut => {
-        console.log(createCard(shortcut))
-   });
+function loadLocalShortcuts(query) {
+    let count = 0;
+    let cardHolder = "";
+
+    const shortcuts = query ? JSON.parse(userData.get("shortcuts")).filter(a => {
+        return a.name.toLowerCase().includes(query.toLowerCase());
+    }) : JSON.parse(userData.get("shortcuts"));
+
+    if(query) {
+        const searchHeader = document.createElement("h4");
+        searchHeader.textContent = `Search results for ${query}`;
+        searchHeader.style.marginTop = "20px";
+        document.querySelector('#shortcutHolder').appendChild(searchHeader);
+    }
+
+    shortcuts.forEach(shortcut => {
+        console.log(shortcut)
+        if(count === 0) {
+            cardHolder = document.createElement("div");
+            cardHolder.classList.add("card-holder");
+        }
+        cardHolder.appendChild(createCard(shortcut));
+        count++;
+        if(count > 1) {
+            document.querySelector('#shortcutHolder').appendChild(cardHolder);
+            count = 0;
+        }
+    });
+
+    // if row has single entry
+    if(count === 1) {
+        document.querySelector('#shortcutHolder').appendChild(cardHolder);
+    }
 }
+
+
+
 
 /**
  * @description creates a shortcut card dom element with the shortcut object provided
@@ -32,15 +74,15 @@ function createCard(shortcut) {
     const head = document.createElement("div");
     head.classList.add("head");
     const heading_container = document.createElement("p");
-    heading_container.innerText = shortcut.carbs;
+    heading_container.innerText = shortcut.carbs + " Carbs";
     head.appendChild(heading_container);
 
     const thumbnail = document.createElement("div");
-    thumbnail.style.backgroundImage = `url('../data/ShortcutLibrary/thumbnails/entry_pizza_140621.jpg')`;
+    thumbnail.classList.add("thumbnail");
+    thumbnail.style.backgroundImage = `url('Data/ShortcutLibrary/thumbnails/${shortcut.img}')`;
 
     const name = document.createElement("p");
-    name.style.padding = "20px";
-    name.innerText = "Slice of pizza";
+    name.innerText = shortcut.name;
     
     const footer = document.createElement("div");
     footer.classList.add("footer");
@@ -49,7 +91,7 @@ function createCard(shortcut) {
     icon.classList.add("fa-plus");
     footer.appendChild(icon);
     const footer_button_container = document.createElement("p");
-    footer_button_container.innerText = shortcut.name;
+    footer_button_container.innerText = "Add";
     footer.appendChild(footer_button_container)
 
     card.appendChild(head);
@@ -65,4 +107,6 @@ function createCard(shortcut) {
  */
 function searchShortcuts() {
     const searchInput = prompt("Enter a search term");
+    document.querySelector('#shortcutHolder').innerHTML = "";
+    loadLocalShortcuts(searchInput);
 }
